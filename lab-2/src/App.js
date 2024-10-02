3ba78d03184ed0492da05fc96b6721cc064996d5
palina-pauliuchenka <ppauliuchenka02@gmail.com>
2024-10-02 19:01:40 -0400

    lab 2

---
+++ lab-2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <svg width={300} height={400}>
+        <line x1='100' y1='100' x2='110' y2='25' stroke='grey'></line>
+      </svg>
+    </div>
+  );
+}
+
+export default App;
